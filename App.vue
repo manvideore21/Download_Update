9af5e92171fc93fada1571a946f26b69a2cbb646
manvideore21 <137396100+manvideore21@users.noreply.github.com>
2024-10-07 01:23:21 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <GoodName greeting="Hello, Manvi!" />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld />
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import GoodName from './components/GoodName.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GoodName,
+    HelloWorld
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
